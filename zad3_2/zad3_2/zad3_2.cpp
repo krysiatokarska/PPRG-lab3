78ee14e9f70e8e46c8f1ebf9befd139fe4edfe86
krysiatokarska <s27440@pjwstk.edu.pl>
2022-11-18 17:11:49 +0100

    poprawka1

--- zad3_2/zad3_2/zad3_2.cpp
+++ zad3_2/zad3_2/zad3_2.cpp
@@ -1,11 +1,6 @@
 ﻿// zad 3_2 Napisz program wyświetlający na ekranie znaki z klawiatury do momentu,
 // aż zostanie podany znak "t"
 
-// Chciałabym żeby automatycznie się kończył,
-// żeby nie trzeba było klikać entera. Czy tak się w ogóle da?
-
-// Nie działa jak wpiszę ciąg znaków mających "t" np. jak wpiszę 'tksjr' to się kończy.
-
 #include <iostream>
 using namespace std;
 
